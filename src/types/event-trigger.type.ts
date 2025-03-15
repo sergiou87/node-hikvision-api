ea6f194a7a96bf677964740a113d21022862b8c1
Sergio Padrino <sergio.padrino@gmail.com>
2025-03-15 21:07:12 +0100

    Set optional fields to EventTrigger

--- src/types/event-trigger.type.ts
+++ src/types/event-trigger.type.ts
@@ -1,45 +1,28 @@
 export type EventTriggerNotification = {
   id: string;
   notificationMethod: string;
-  notificationRecurrence: string;
-  notificationInterval: number;
-  outputIOPortID: string;
-  dynOutputIOPortID: string;
-  videoInputID: string;
-  dynVideoInputID: string;
-  ptzAction: {
+  notificationRecurrence?: string;
+  notificationInterval?: number;
+  outputIOPortID?: string;
+  dynOutputIOPortID?: string;
+  videoInputID?: string;
+  dynVideoInputID?: string;
+  ptzAction?: {
     ptzChannelID: string;
     actionName: string;
-    actionNum: number;
+    actionNum?: number;
   };
 }
 
 export type EventTrigger = {
-  enabled: boolean;
-  enableHighlight: boolean;
-  samplingInterval: number;
-  startTriggerTime: number;
-  endTriggerTime: number;
-  regionType: string;
-  Grid: {
-    rowGranularity: number;
-    columnGranularity: number;
-  };
-  MotionDetectionLayout: {
-    sensitivityLevel: number;
-    layout: {
-      gridMap: string;
-    };
-    targetType: string;
-  };
   id: string;
   eventType: string;
-  eventDescription: string;
-  inputIOPortID: string;
-  dynInputPortID: string;
-  videoInputChannelID: string;
-  dynVideoInputChannelID: string;
-  intervalBetweenEvents: number;
-  WLSensorID: string;
-  EventTriggerNotificationList: EventTriggerNotification[];
+  eventDescription?: string;
+  inputIOPortID?: string;
+  dynInputPortID?: string;
+  videoInputChannelID?: string;
+  dynVideoInputChannelID?: string;
+  intervalBetweenEvents?: number;
+  WLSensorID?: string;
+  EventTriggerNotificationList?: EventTriggerNotification[];
 };
